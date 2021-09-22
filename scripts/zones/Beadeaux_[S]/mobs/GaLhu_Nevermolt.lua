d5b558db479105d1fb20b04aa77af942d354c2c4
DESKTOP-976J67T\Connor <kubiakconnor@gmail.com>
2021-09-22 18:45:07 -0400

    Created a lua file for Ga'Lhu Nevermolt

---
+++ scripts/zones/Beadeaux_[S]/mobs/GaLhu_Nevermolt.lua
@@ -0,0 +1,10 @@
+-----------------------------------
+-- Area: Beadeaux [S] (92)
+-- NM: Ga'Lhu Nevermolt
+-- !pos 208.697 -3.353 -39.247
+-----------------------------------
+mixins = {require("scripts/mixins/job_special")}
+require("scripts/globals/mobs")
+require("scripts/globals/status")
+-----------------------------------
+
