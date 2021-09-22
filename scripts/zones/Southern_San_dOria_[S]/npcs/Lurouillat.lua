8a23dd1ade39b6c444f1b19ef3eca8a4551f3d13
DESKTOP-976J67T\Connor <kubiakconnor@gmail.com>
2021-09-21 23:06:10 -0400

    Added Lurouillat trade functionality

--- scripts/zones/Southern_San_dOria_[S]/npcs/Lurouillat.lua
+++ scripts/zones/Southern_San_dOria_[S]/npcs/Lurouillat.lua
@@ -4,7 +4,67 @@
 -- !pos 44 2 -35 80
 -----------------------------------
 
+local ID = require("scripts/zones/Southern_San_dOria_[S]/IDs")
+require("scripts/globals/keyitems")
+require("scripts/globals/settings")
+
 function onTrade(player, npc, trade)
+    -- Trade boxes from La Vaule (S)
+    if (trade:hasItemQty(2678, 1) and trade:getItemCount() == 1) then -- Maple Strongbox
+        player:tradeComplete()
+        player:addKeyItem(tpz.ki.IMPERIAL_LINEAGE_CHAPTER_I)
+        player:messageSpecial(ID.text.KEYITEM_OBTAINED, tpz.ki.IMPERIAL_LINEAGE_CHAPTER_I)
+ -- correct onTradeEvent here, possibly 1 or multiple events per strongbox     player:startEvent(number here)
+    end
+
+    if (trade:hasItemQty(2679, 1) and trade:getItemCount() == 1) then -- Magnolia Strongbox
+        player:tradeComplete()
+        player:addKeyItem(tpz.ki.IMPERIAL_LINEAGE_CHAPTER_II)
+        player:messageSpecial(ID.text.KEYITEM_OBTAINED, tpz.ki.IMPERIAL_LINEAGE_CHAPTER_II)
+ -- correct onTradeEvent here, possibly 1 or multiple events per strongbox     player:startEvent(number here)
+    end
+
+    if (trade:hasItemQty(2680, 1) and trade:getItemCount() == 1) then -- Beech Strongbox
+        player:tradeComplete()
+        player:addKeyItem(tpz.ki.IMPERIAL_LINEAGE_CHAPTER_III)
+        player:messageSpecial(ID.text.KEYITEM_OBTAINED, tpz.ki.IMPERIAL_LINEAGE_CHAPTER_III)
+ -- correct onTradeEvent here, possibly 1 or multiple events per strongbox     player:startEvent(number here)
+    end
+
+    if (trade:hasItemQty(2681, 1) and trade:getItemCount() == 1) then -- Evergreen Strongbox
+        player:tradeComplete()
+        player:addKeyItem(tpz.ki.IMPERIAL_LINEAGE_CHAPTER_IV)
+        player:messageSpecial(ID.text.KEYITEM_OBTAINED, tpz.ki.IMPERIAL_LINEAGE_CHAPTER_IV)
+ -- correct onTradeEvent here, possibly 1 or multiple events per strongbox     player:startEvent(number here)
+    end
+
+    if (trade:hasItemQty(2682, 1) and trade:getItemCount() == 1) then -- Holly Strongbox
+        player:tradeComplete()
+        player:addKeyItem(tpz.ki.IMPERIAL_LINEAGE_CHAPTER_V)
+        player:messageSpecial(ID.text.KEYITEM_OBTAINED, tpz.ki.IMPERIAL_LINEAGE_CHAPTER_V)
+ -- correct onTradeEvent here, possibly 1 or multiple events per strongbox     player:startEvent(number here)
+    end
+
+    if (trade:hasItemQty(2683, 1) and trade:getItemCount() == 1) then -- Oak Strongbox
+        player:tradeComplete()
+        player:addKeyItem(tpz.ki.IMPERIAL_LINEAGE_CHAPTER_VI)
+        player:messageSpecial(ID.text.KEYITEM_OBTAINED, tpz.ki.IMPERIAL_LINEAGE_CHAPTER_VI)
+ -- correct onTradeEvent here, possibly 1 or multiple events per strongbox     player:startEvent(number here)
+    end
+
+    if (trade:hasItemQty(2684, 1) and trade:getItemCount() == 1) then -- Elm Strongbox
+        player:tradeComplete()
+        player:addKeyItem(tpz.ki.IMPERIAL_LINEAGE_CHAPTER_VII)
+        player:messageSpecial(ID.text.KEYITEM_OBTAINED, tpz.ki.IMPERIAL_LINEAGE_CHAPTER_VII)
+ -- correct onTradeEvent here, possibly 1 or multiple events per strongbox     player:startEvent(number here)
+    end
+
+    if (trade:hasItemQty(2685, 1) and trade:getItemCount() == 1) then -- Willow Strongbox
+        player:tradeComplete()
+        player:addKeyItem(tpz.ki.IMPERIAL_LINEAGE_CHAPTER_VIII)
+        player:messageSpecial(ID.text.KEYITEM_OBTAINED, tpz.ki.IMPERIAL_LINEAGE_CHAPTER_VIII)
+ -- correct onTradeEvent here, possibly 1 or multiple events per strongbox     player:startEvent(number here)
+    end
 end
 
 function onTrigger(player, npc)
