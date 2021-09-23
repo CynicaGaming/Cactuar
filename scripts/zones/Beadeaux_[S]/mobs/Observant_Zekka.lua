e28c65cfbd3eb3aef60b1a7fd88c69233765da8e
DESKTOP-976J67T\Connor <kubiakconnor@gmail.com>
2021-09-23 19:42:06 -0400

    Created Observant Zekka's lua, added immunities to Observant Zekka

--- scripts/zones/Beadeaux_[S]/mobs/Observant_Zekka.lua
+++ scripts/zones/Beadeaux_[S]/mobs/Observant_Zekka.lua
@@ -4,6 +4,9 @@
 -- !pos 128 0.4 161
 -----------------------------------
 mixins = {require("scripts/mixins/families/imp")}
+require("scripts/globals/mobs")
+require("scripts/globals/status")
+require("scripts/mixins/job_special")
 -----------------------------------
 
 function onMobDeath(mob, player, isKiller)
