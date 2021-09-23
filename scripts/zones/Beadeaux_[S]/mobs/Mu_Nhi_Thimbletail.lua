fd63ecfe0c6889c9ed8eb8a09a7336bf69f2a0fa
DESKTOP-976J67T\Connor <kubiakconnor@gmail.com>
2021-09-23 19:46:05 -0400

    Added Mu'Nhi Thimbletail's lua (no immunities)

---
+++ scripts/zones/Beadeaux_[S]/mobs/Mu_Nhi_Thimbletail.lua
@@ -0,0 +1,13 @@
+-----------------------------------
+-- Area: Beadeaux [S]
+--  Mob: Mu'Nhi Thimbletail
+-- !pos   240.284 -3.717 119.684
+-----------------------------------
+mixins =
+require("scripts/globals/mobs")
+require("scripts/globals/status")
+require("scripts/mixins/job_special")
+-----------------------------------
+
+function onMobDeath(mob, player, isKiller)
+end
