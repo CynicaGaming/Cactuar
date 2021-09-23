1ac650939b72de49ed0d2bc367c08e2cfb35f06e
DESKTOP-976J67T\Connor <kubiakconnor@gmail.com>
2021-09-23 19:58:28 -0400

    added Blifnix Oilycheeks lua, added immunities to Blifnix Oilycheeks

---
+++ scripts/zones/Beadeaux_[S]/mobs/Blifnix_Oilycheeks.lua
@@ -0,0 +1,8 @@
+-----------------------------------
+-- Area: Beadeaux [S]
+--   NM: Ba'Tho Mercifulheart
+-- pos -132.780 23.532 97.215
+-----------------------------------
+mixins = {require("scripts/mixins/job_special")}
+require("scripts/globals/mobs")
+require("scripts/globals/status")
