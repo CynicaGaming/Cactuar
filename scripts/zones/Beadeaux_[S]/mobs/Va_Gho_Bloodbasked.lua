0ecf9ab406b9147c7dbe6190d26dfcc2f9dcf6e9
DESKTOP-976J67T\Connor <kubiakconnor@gmail.com>
2021-09-23 20:14:19 -0400

    added lua's for Ra'Dha Scarscute and Va'Gho Bloodbasked, added immunities to Va'Gho Bloodbasked

---
+++ scripts/zones/Beadeaux_[S]/mobs/Va_Gho_Bloodbasked.lua
@@ -0,0 +1,13 @@
+-----------------------------------
+-- Area: Beadeaux [S]
+--  Mob: Va'Gho Bloodbasked
+-- !pos  48.226, -3.267, 41.630
+-----------------------------------
+mixins =
+require("scripts/globals/mobs")
+require("scripts/globals/status")
+require("scripts/mixins/job_special")
+-----------------------------------
+
+function onMobDeath(mob, player, isKiller)
+end
