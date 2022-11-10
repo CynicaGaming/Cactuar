181627878c20e38ca10293f017daeea664479d97
Umeboshi <terrandragon@gmail.com>
2022-11-10 12:45:06 -0800

    Aspi/Fafnir fix
    (cherry picked from commit f1d051eb38135b006930815cff648a5dff7b1cb1)

--- scripts/zones/Dragons_Aery/mobs/Nidhogg.lua
+++ scripts/zones/Dragons_Aery/mobs/Nidhogg.lua
@@ -49,7 +49,7 @@ end
 function onMobDespawn(mob)
 
     if LandKingSystem_NQ > 0 or LandKingSystem_HQ > 0 then
-        GetNPCByID(ID.npc.BEHEMOTH_QM):setStatus(tpz.status.Normal)
+        GetNPCByID(ID.npc.FAFNIR_QM):setStatus(tpz.status.Normal)
     end
     
     -- Set Nidhogg's Window Open Time
